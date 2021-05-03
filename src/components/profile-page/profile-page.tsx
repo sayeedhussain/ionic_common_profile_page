47bc755891ac6d5965c46242a442ce9df0c16915
Shanu Madassery <shanum@thoughtworks.com>
2021-05-03 17:52:12 +0530

    Shanu: Modify the page to show person details

--- src/components/profile-page/profile-page.tsx
+++ src/components/profile-page/profile-page.tsx
@@ -20,17 +20,32 @@ export class ProfilePage {
           <h3 class="detail-heading">Basic Details</h3>
           <div>
             <div class="inline">
-              <div>Name</div>
+              <div class="label">Name</div>
               <div>Marco</div>
             </div>
             <div class="inline">
-              <div>Date Of Birth</div>
+              <div class="label">Date Of Birth</div>
               <div>02-Nov-1979</div>
             </div>
           </div>
         </div>
         <div>
           <h3 class="detail-heading">Contanct Details</h3>
+          <div>
+            <div class="inline">
+              <div class="label">Primay Email</div>
+              <div>marcovalt@gmail.com</div>
+            </div>
+            <div class="inline">
+              <div class="label">Alternate Email</div>
+              <div>N/A</div>
+            </div>
+            <div class="inline">
+              <div class="label">Mobile</div>
+              <div>+39 874 274 9482</div>
+            </div>
+          </div>
+
         </div>
       </div>
     );
